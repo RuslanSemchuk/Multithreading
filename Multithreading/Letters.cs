3b73bac00cedbc6a0c34fa8bc9500f93673f28c7
RuslanSemchuk <134958046+RuslanSemchuk@users.noreply.github.com>
2023-09-18 18:57:16 +0300

    Add program cs

---
+++ Multithreading/Letters.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    private static Mutex mutexA = new Mutex();
+    private static Mutex mutexB = new Mutex();
+    private static Mutex mutexC = new Mutex();
+
+    static void Main()
+    {
+        Thread threadA = new Thread(PrintA);
+        Thread threadB = new Thread(PrintB);
+        Thread threadC = new Thread(PrintC);
+
+        threadA.Start();
+        threadB.Start();
+        threadC.Start();
+
+        threadA.Join();
+        threadB.Join();
+        threadC.Join();
+
+    }
+    static void PrintC()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            mutexA.WaitOne();
+            Console.WriteLine("C");
+            mutexA.ReleaseMutex();
+        }
+    }
+    static void PrintA()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            mutexA.WaitOne();
+            Console.WriteLine("A");
+            mutexA.ReleaseMutex();
+        }
+    }
+
+    static void PrintB()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            mutexA.WaitOne();
+            Console.WriteLine("B");
+            mutexA.ReleaseMutex();
+        }
+    }
+
+   
+}
---
+++ Multithreading/Numbers.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Threading;
+
+//class Numbers
+//{
+//    private static Semaphore evenSemaphore = new Semaphore(1, 1);
+//    private static Semaphore oddSemaphore = new Semaphore(0, 1);
+
+//    static void Main()
+//    {
+//        Thread evenThread = new Thread(PrintEvenNumbers);
+//        Thread oddThread = new Thread(PrintOddNumbers);
+
+//        evenThread.Start();
+//        oddThread.Start();
+
+//        evenThread.Join();
+//        oddThread.Join();
+
+//    }
+
+//    static void PrintEvenNumbers()
+//    {
+//        for (int i = 2; i <= 10; i += 2)
+//        {
+//            evenSemaphore.WaitOne();
+//            Console.WriteLine(i);
+//            Thread.Sleep(1000);
+//            oddSemaphore.Release();
+//        }
+//    }
+
+//    static void PrintOddNumbers()
+//    {
+//        for (int i = 1; i <= 9; i += 2)
+//        {
+//            oddSemaphore.WaitOne();
+//            Console.WriteLine(i);
+//            Thread.Sleep(1000);
+//            evenSemaphore.Release();
+//        }
+//    }
+//}
